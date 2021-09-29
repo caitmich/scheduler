65f1d58472e37a026d2635674d3e2ef20d5ac90b
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-09-29 16:14:23 -0400

    add appointment folder with index.js file

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
 
 //stories for buttons
 storiesOf("Button", module)
@@ -134,3 +135,12 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+//stories for Appointment component
+
+storiesOf("Appointment", module)
+.addParameters({
+  backgrounds: [{name: "white", value: "#fff", default: true}]
+})
+.add("Appointment", () => <Appointment/>)
+.add("Appointment with Time", () => <Appointment time="12pm" />)
